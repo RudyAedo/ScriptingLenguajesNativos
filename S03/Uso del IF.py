d394774fcec9f9baed14c0dd2dd15b518ad85594
Rudy <rudy.aedo.zea@gmail.com>
2025-07-30 23:04:04 -0500

    Create Uso del IF.py

---
+++ S03/Uso del IF.py
@@ -0,0 +1,14 @@
+#Uso del IF cuando es igual
+edad = 25
+if edad == 17:
+    print('Es igual a 17')
+
+#Uso del IF cuando es mayor
+edad = 25
+if edad > 17:
+    print('Es mayor a 17')
+
+#Uso del IF cuando es menor
+edad = 25
+if edad < 17:
+    print('Es menor a 17')
