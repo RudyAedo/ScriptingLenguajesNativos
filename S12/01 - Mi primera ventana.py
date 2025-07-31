e8d0f5d68b368a636e5534558678e26ad548ca2a
Rudy <rudy.aedo.zea@gmail.com>
2025-07-31 10:18:04 -0500

    Create 01 - Mi primera ventana.py

---
+++ S12/01 - Mi primera ventana.py
@@ -0,0 +1,15 @@
+import maya.cmds as cmds
+
+# Este será el nombre que usará la ventana que crearemos
+win='miVentana'
+
+# Valida si la ventana existe para tener una sola instancia de la misma
+if cmds.window(win, exists= True):
+    cmds.deleteUI(win, window= True)
+
+# El flag 's' permite que la ventana se pueda redimensionar
+cmds.window(win,t='Ventana',w=300,h=150,s= False,mxb=False,bgc=(0.6,0.6,0.6))
+
+#cmds.columnLayout(w=150,h=150)  # Esto es lo que hace que Maya respete el tamaño
+
+cmds.showWindow(win)
