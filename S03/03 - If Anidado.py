b4cfc6f7b0447db4f48eee4cd3a5487cd113bf89
Rudy <rudy.aedo.zea@gmail.com>
2025-07-30 23:35:58 -0500

    Create 03 - If Anidado.py

---
+++ S03/03 - If Anidado.py
@@ -0,0 +1,11 @@
+# If anidado
+usuario = 'Pepito'
+password = 123
+
+if usuario == 'Pepito':
+    if password == 123:
+        print('Bienvenido')
+    else:
+        print('Contraseña incorrecta')
+else:
+    print('Usuario no encontrado')
