910492027349b1538a01c2d8e4ee693c797f0ea8
Rudy <rudy.aedo.zea@gmail.com>
2025-07-31 15:25:00 -0500

    Create 02 - Column Layout.py

---
+++ S12/02 - Column Layout.py
@@ -0,0 +1,21 @@
+import maya.cmds as cmds
+
+win='miVentana'
+
+if cmds.window(win, exists=True):
+    cmds.deleteUI(win, window=True)
+
+cmds.window(win,t='Column Layout',
+    w=400,h=500,s=True,
+    mxb=False,bgc=(0.6,0.6,0.6))
+
+cmds.columnLayout(adj=True) #Layout tipo columna----------
+cmds.button(l='Botón 1',bgc=[0.3,0.3,0.3],h=50)
+cmds.button(l='Botón 2',bgc=[0.3,0.3,0.3],h=50)
+cmds.button(l='Botón 3',bgc=[0.3,0.3,0.3],h=50)
+cmds.button(l='Botón 4',bgc=[0.3,0.3,0.3],h=50)
+cmds.button(l='Botón 5',bgc=[0.3,0.3,0.3],h=50)
+cmds.button(l='Botón 6',bgc=[0.3,0.3,0.3],h=50)
+cmds.setParent('..') #Cierre del layout------------------
+
+cmds.showWindow(win)
