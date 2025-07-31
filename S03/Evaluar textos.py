39294ebb125639e6590cbf9f2e567608796b8911
Rudy <rudy.aedo.zea@gmail.com>
2025-07-30 23:15:27 -0500

    Create Evaluar textos.py

---
+++ S03/Evaluar textos.py
@@ -0,0 +1,14 @@
+# Evaluar Textos
+nombre = 'Tulio'
+if len(nombre) > len('Juanin'):
+    print('Tiene más caracteres')
+else:
+    print('Tiene menos caracteres')
+
+
+#Evaluar Textos con LEN
+nombre = 'Tulio'
+if len(nombre) > 4:
+    print('Es mayor de 4')
+else:
+    print('Es menor de 4')
