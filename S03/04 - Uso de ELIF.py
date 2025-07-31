f7dbb7e406e02d7d6a73b8aeef0a90f10cce94fb
Rudy <rudy.aedo.zea@gmail.com>
2025-07-30 23:38:55 -0500

    Create 04 - Uso de ELIF.py

---
+++ S03/04 - Uso de ELIF.py
@@ -0,0 +1,14 @@
+#Uso del IF con ELIF
+
+color = 'rojo'
+
+if color == 'rojo':
+    print('Es Rojo')
+elif color == 'blanco':
+    print ('Es Blanco')
+elif color == 'verde':
+    print ('Es Verde')
+elif color == 'amarillo':
+    print ('Es Amarillo')
+else:
+    print ('No hay color')
