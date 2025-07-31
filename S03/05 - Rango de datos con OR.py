129fc9291bfcc4858b1184cd10b3b36d519ba027
Rudy <rudy.aedo.zea@gmail.com>
2025-07-30 23:44:39 -0500

    Create 05 - Rango de datos con OR.py

---
+++ S03/05 - Rango de datos con OR.py
@@ -0,0 +1,9 @@
+# Uso de Rango de Datos con OR
+# Evalua si el rango esta fuera de los límites establecidos
+
+numero = 75  # Puedes cambiar este valor para probar
+
+if numero < 0 or numero >= 50:
+    print('Fuera del rango')
+else:
+    print('Dentro del rango')
