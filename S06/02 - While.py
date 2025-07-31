b859b667feb9ae0177a52405ebbca17a55e11127
Rudy <rudy.aedo.zea@gmail.com>
2025-07-31 09:22:43 -0500

    Create 02 - While.py

---
+++ S06/02 - While.py
@@ -0,0 +1,8 @@
+# Imprimir los números del 1 al 5
+contador = 1
+
+while contador <= 5:
+    print(contador)
+  
+    #El contador debe incrmentar para que se cumpla la condición
+    contador += 1 
